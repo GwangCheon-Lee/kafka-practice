4d383b884b8fb16bc44dda66bee8ad5f0c5d06cb
gclee <raitmid56@gmail.com>
2024-01-17 15:23:28 +0900

    feat: Add RoundRobin partitioning and batching to Kafka producer

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoWithCallback.java
@@ -22,33 +22,44 @@ public class ProducerDemoWithCallback {
 				properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 				properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+				properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, "400");
+				properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, RoundRobinPartitioner.class.getName());
+
 				// create the Producer
 				KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-				for (int i = 0; i < 10; i++) {
-						// create Producer Record
-						ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world " + i);
+				for (int j = 0; j < 10; j++) {
+						for (int i = 0; i < 30; i++) {
+								// create Producer Record
+								ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello world " + i);
 
-						// send data
-						producer.send(producerRecord, new Callback() {
-								@Override
-								public void onCompletion(RecordMetadata metadata, Exception e) {
-										// executes every time a record successfully sent or an exception is thrown
-										if (e == null) {
-												//	the record was successfully sent
-												log.info("Received new metadata \n" +
-																"Topic: " + metadata.topic() + "\n" +
-																"Partition: " + metadata.partition() + "\n" +
-																"Offset: " + metadata.offset() + "\n" +
-																"Timestamp: " + metadata.timestamp() + "\n"
-												);
+								// send data
+								producer.send(producerRecord, new Callback() {
+										@Override
+										public void onCompletion(RecordMetadata metadata, Exception e) {
+												// executes every time a record successfully sent or an exception is thrown
+												if (e == null) {
+														//	the record was successfully sent
+														log.info("Received new metadata \n" +
+																		"Topic: " + metadata.topic() + "\n" +
+																		"Partition: " + metadata.partition() + "\n" +
+																		"Offset: " + metadata.offset() + "\n" +
+																		"Timestamp: " + metadata.timestamp() + "\n"
+														);
 
-										} else {
-												log.error("Error while producing", e);
+												} else {
+														log.error("Error while producing", e);
+												}
 										}
-								}
-						});
+								});
+						}
+						try {
+								Thread.sleep(500);
+						} catch (InterruptedException e) {
+								throw new RuntimeException(e);
+						}
 				}
+
 
 				// tell the producer to send all data and block util done -- synchronous
 				producer.flush();
